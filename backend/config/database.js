0d11eb3cf8211f48bf860972491aa85d9287bb64
MDROKONMIA <miarokon2001@gmail.com>
2023-05-03 18:22:35 +0600

    FINAL

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ const mongoose= require('mongoose');
 const connectDatabase=()=>{ 
    mongoose
      .connect(
-       process.env.DB_URL, 
+       process.env.DB_URI, 
        {
          useNewUrlParser: true,
          useUnifiedTopology: true,
